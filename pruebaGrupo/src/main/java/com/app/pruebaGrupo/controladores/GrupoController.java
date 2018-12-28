a26ffe3bf304fd04fec9680d20c08594b27106ef
camilo <ikamc014@gmail.com>
2018-12-28 17:35:14 -0500

    implementacion crossOrigin para otorgar permisos a otros servidores

--- pruebaGrupo/src/main/java/com/app/pruebaGrupo/controladores/GrupoController.java
+++ pruebaGrupo/src/main/java/com/app/pruebaGrupo/controladores/GrupoController.java
@@ -3,40 +3,42 @@ package com.app.pruebaGrupo.controladores;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.app.pruebaGrupo.modelos.Grupo;
 import com.app.pruebaGrupo.modelos.GrupoDao;
 
-@Controller
+@RestController
 public class GrupoController {
 	@Autowired
 	private GrupoDao grupoDao;
 	
-	 @RequestMapping("/api/grupos/get-by-codPadre")
-	  @ResponseBody
-	  public List<Grupo> getByCodPadre(Long codPadre) {
-		 List<Grupo> lst = null;
+	@CrossOrigin
+	@GetMapping(value="/api/grupos/get-by-codPadre",produces=MediaType.APPLICATION_JSON_VALUE)	  
+	public List<Grupo> getByCodPadre(Long codPadre) {
+		List<Grupo> lst = null;
 	    try {
 	    	if (codPadre != null){
 	    		lst = grupoDao.findByCodPadre(codPadre);
-	    		
 	    	}
 	    	else
 	    		lst = grupoDao.findByCodPadre(null);
-	    		
 	    }
 	    catch (Exception ex) {
 	      return null;
 	    }
 	    return lst;
-	  }
+	 }
 	 
-	 @RequestMapping("/api/grupos/get-all")
-	  @ResponseBody
-	  public List<Grupo> getAll() {
+	 @CrossOrigin 
+	 @GetMapping(value="/api/grupos/get-all",produces=MediaType.APPLICATION_JSON_VALUE)
+	 public List<Grupo> getAll() {
 		 List<Grupo> lst = null;
 	    try {
 	    	lst = grupoDao.findAll();
@@ -45,6 +47,6 @@ public class GrupoController {
 	      return null;
 	    }
 	    return lst;
-	  }
+	 }
 
 }
